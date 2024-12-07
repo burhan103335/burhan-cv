5875a65803ff67db68cd290cd7a06398c279fe38
Burhan Uddin <34088095+burhan103335@users.noreply.github.com>
2024-12-06 19:38:11 -0500

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Burhan Uddin</h1>
+      <p>Software Engineer | C++ | Python | Web Development</p>
+    </header>
+  )
+}
+
+export default Header
